673325303c92803cd4dd9dceb3362f5b2a59f235
shervindale <shervindalemaupotabern@gmail.com>
2026-03-13 15:03:38 +0800

    AI Explanation feature done

---
+++ PocketClass_AIFeatures/types/input/index.ts
@@ -0,0 +1,7 @@
+export type { AIExplanationInput }      from './AIExplanationInput';
+export type { DiagnosticQuizInput }     from './diagnosticQuizInput';
+export type { diagnosticScoreInterface }from './diagnosticScoreInterface';
+export type { LessonMaterialInput }     from './LessonMaterialInput';
+export type { LessonQuizInput, Question }from './LessonQuizInput';
+export type { TutorChatInput, ChatHistoryEntry } from './TutorChatInput';
+export type { QuarterlyExamInput }      from './quarterlyExamInput';
